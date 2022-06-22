17d4afca64b58f04e316a872f0b66ced3f896cf0
Guido Giuntoli <gagiuntoli@gmail.com>
2022-06-22 23:02:05 +0200

    Adding key expansion algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use core::convert::TryInto;
 use std::ops;
-extern crate num;
+use num;
 
 macro_rules! rotl {
     ($a: expr, $b: expr, $w: expr, $w_minus_1: expr) => {
@@ -21,56 +21,99 @@ macro_rules! rotr {
 //const c: usize = 4;  // number of words in a key
 //const t: u32 = 2*(r+1); // table size (26)
 //let S[t]: Word;
+    // P = 0xb7e1;                 Q = 0x9e37              // w = 16
+    // P = 0xb7e15163;             Q = 0x9e3779b9          // w = 32
+    // P = 0xb7e151628aed2a6b;     Q = 0x9e3779b97f4a7c15; // w = 64
 
-    /*
-     * Expands the key to t = 2(r+1) bytes
-     */
-//    fn expand_key(key: Vec<u8>) -> [u8; T] {
-//        // P = 0xb7e1;                 Q = 0x9e37              // w = 16
-//        // P = 0xb7e15163;             Q = 0x9e3779b9          // w = 32
-//        // P = 0xb7e151628aed2a6b;     Q = 0x9e3779b97f4a7c15; // w = 64
-//        let mut L: [W; c];
-//        const u: u32 = w/4;
-//        let mut ekey = [0u8; T];
-//        L[c-1] = 0;
-//        for i in (0..=(b-1)).rev() {
-//            println!("{}",i);
-//            //L[(i/u) as usize] = (L[(i/u) as usize] << 8) + key[i as usize];
-//        }
-//        return ekey
-//    }
-//}
+trait Unsigned: num::Unsigned +
+num::traits::WrappingAdd +
+std::ops::BitAnd<Output = Self> +
+std::ops::BitOr<Output = Self> +
+std::ops::BitXor<Output = Self> +
+std::ops::Shl<Output = Self> +
+std::ops::Shr<Output = Self>
+{
+    const BITS: u32;
+}
+
+impl Unsigned for u16 {
+    const BITS: u32 = u16::BITS;
+}
+
+impl Unsigned for u32 {
+    const BITS: u32 = u32::BITS;
+}
+
+impl Unsigned for u64 {
+    const BITS: u32 = u64::BITS;
+}
 
 fn encode<W, const T: usize>(key_exp: [W; T], pt: [W; 2]) -> [W; 2]
 where
-    W:
-    ops::Add<Output = W> +
-    ops::Sub<Output = W> +
-    ops::BitAnd<Output = W> +
-    ops::BitXor<Output = W> +
-    ops::Shl<Output = W> +
-    ops::Shr<Output = W> +
-    ops::BitOr<Output = W> +
-    Copy + 
-    From<u32>
+    W: Unsigned + From<u32> + Copy
 {
     let r = T/2 - 1;
     let mut a = pt[0] + key_exp[0];
     let mut b = pt[1] + key_exp[1];
     for i in 1..=r {
-        a = rotl!(a^b, b, W::from(32), W::from(31)) + key_exp[2*i];
-        b = rotl!(b^a, a, W::from(32), W::from(31)) + key_exp[2*i+1];
+        a = rotl!(a^b, b, W::from(W::BITS), W::from(W::BITS-1)) + key_exp[2*i];
+        b = rotl!(b^a, a, W::from(W::BITS), W::from(W::BITS-1)) + key_exp[2*i+1];
     }
     [a,b]
 }
 
-fn expand_key<W, const T: usize>(key: Vec<u8>) -> [W;T]
+/*
+ * Expands the key to t = 2(r+1) bytes
+ */
+#[allow(arithmetic_overflow)]
+fn expand_key<W, const T: usize>(key: Vec<u8>, p: W, q: W) -> [W;T]
 where
-    W: From<u32> +
-    std::marker::Copy
+    W: Unsigned + From<u8> + From<u32> + std::marker::Copy + std::fmt::Debug
 {
-    let mut key_exp = [W::from(0); T];
-    key_exp
+
+    let mut key_s = [W::from(0u8); T];
+    let b = key.len();
+    let r = T/2 - 1;
+
+    // c = max(1, ceil(8*b/w))
+    let c = (std::cmp::max(
+            1, (8*key.len() + u32::BITS as usize - 1) as u32 / u32::BITS)
+            ) as usize;
+    println!("c = {}", c);
+
+    // converting the secrey key from bytes to words
+    let mut key_l = [W::from(0u8); 100];
+    let u = (W::BITS / 8) as usize;
+    for i in (0..=(b-1)).rev() {
+        let ix = (i/u) as usize;
+        key_l[ix] = (key_l[ix]<<W::from(8u8)).wrapping_add(&W::from(key[i]));
+    }
+    println!("L = {:?}", key_l);
+    
+    // initializing array S
+    key_s[0] = p;
+    for i in 1..=(T-1) {
+        key_s[i] = key_s[i-1] + q;
+    }
+    println!("key_s = {:?}", key_s);
+
+    // Mixing in the secret key
+    let mut i = 0;
+    let mut j = 0;
+    let mut a = W::from(0u8);
+    let mut b = W::from(0u8);
+    for k in 0..std::cmp::max(c, T) {
+        key_s[i] = (key_s[i] + a + b) << W::from(3u8);
+        a = key_s[i];
+        key_l[i] = (key_l[i] + a + b) << (a + b);
+        b = key_l[i];
+        i = (i+1)%T;
+        j = (j+1)%c;
+    }
+
+    println!("key_s = {:?}", key_s);
+
+    key_s
 }
 
 /*
@@ -95,11 +138,12 @@ mod tests {
     #[test]
     fn encode_a() {
     	let key = vec![0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F];
-    	let key_exp = [0x00010203, 0x04050607, 0x00010203, 0x04050607];
+    	//let key_exp = [0x00010203, 0x04050607, 0x00010203, 0x04050607];
     	let pt  = vec![0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77];
     	let ct  = vec![0x2D, 0xDC, 0x14, 0x9B, 0xCF, 0x08, 0x8B, 0x9E];
-    	let res = encode::<u32, 4>(key_exp, [0x0001020304, 0x05060708]);
-    	assert!(&ct[..] == &res[..]);
+        let key_s = expand_key::<u32, 26>(key, 0xb7e15163, 0x9e3779b9);
+    	//let res = encode::<u32, 4>(key_s, [0x0001020304, 0x05060708]);
+    	//assert!(&ct[..] == &res[..]);
     }
 
     //#[test]
